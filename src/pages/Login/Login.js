e1840d8021c26b0cc4971dfe9317356de137491c
Namit Doshi <namitdoshi90@gmail.com>
2020-09-04 00:01:37 +0530

    login component added

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-sm-12 col-md-6'>
+          <p>asldkadlkasdk;akd;kad;kas;dka;kd;aksdkas;dkas;k;k</p>
+        </div>
+        <div className='col-sm-12 col-md-6'>
+          <p>asldkadlkasdk;akd;kad;kas;dka;kd;aksdkas;dkas;k;k</p>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Login;
