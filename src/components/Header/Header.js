33c5b7d24f104ba47fe0c573b71e81e7299fad44
Namit Doshi <namitdoshi90@gmail.com>
2020-09-03 22:36:39 +0530

    header created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md" fixed="top">
+        <NavbarBrand href="/">Job Portal</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="#">About us</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Login</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/Landing-Page/Landing-Page.js
+++ src/pages/Landing-Page/Landing-Page.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Header from '../../components/Header/Header'
+
 const LandingPage = () => {
   return (
     <div>
-      <p>dasdsadsa</p>
+      <Header />
     </div>
   )
 }
