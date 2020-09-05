f0efb6ba980ee39c1055865c69127d9f86c9ca72
Namit Doshi <namitdoshi90@gmail.com>
2020-09-05 16:13:39 +0530

    job seeker signup api linked

--- src/pages/Job-Seeker-Signup/Job-Seeker-Signup.js
+++ src/pages/Job-Seeker-Signup/Job-Seeker-Signup.js
@@ -1,31 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import axios from 'axios';
 
 import './Job-Seeker-Signup.scss';
 
-const JobSeekerSignup = () => {
-  return (
-    <div className='container signup-container'>
-      <div className='row'>
-        <div className='col-12 col-sm-12 col-md-12'>
-          <h2>Welcome to Job Seeker Platform!</h2>
-          <h4>Sign Up for a account here</h4>
-        <Form>
-          <FormGroup>
-            <Label for="email">Email</Label>
-            <Input type="email" name="email" id="email" placeholder="your email here" required />
-          </FormGroup>
-          <FormGroup>
-            <Label for="examplePassword">Password</Label>
-            <Input type="password" name="password" id="examplePassword" placeholder="your password here" required />
-          </FormGroup>
-          <Button color='success'>Sign Up</Button>
-        </Form>
-        </div>
+class JobSeekerSignup extends Component {
+  
+  constructor () {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
 
+  handleChange = (event) => {
+    const {value,name} = event.target
+
+    this.setState({ [name]: value })
+    console.log(this.state)
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // let apiBaseUrl = "http://localhost:3000/";
+    let payload = {
+      "email": this.state.email,
+      "password": this.state.password
+    };
+    
+    axios.post('/signup-job-seeker', payload)
+    .then(response => {
+      console.log(response)
+    });
+
+  }
+
+  render () {
+    return (
+      <div className='container signup-container'>
+        <div className='row'>
+          <div className='col-12 col-sm-12 col-md-12'>
+            <h2>Welcome to Job Seeker Platform!</h2>
+            <h4>Sign Up for a account here</h4>
+          <Form>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input type="email" name="email" id="email" placeholder="your email here" onChange={this.handleChange} required />
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input type="password" name="password" id="examplePassword" placeholder="your password here" onChange={this.handleChange} required />
+            </FormGroup>
+            <Button color='success' onClick={this.handleSubmit} >Sign Up</Button>
+          </Form>
+          </div>
+
+          </div>
         </div>
-      </div>
-  )
-};
+    )
+  };
+}
 
 export default JobSeekerSignup;
