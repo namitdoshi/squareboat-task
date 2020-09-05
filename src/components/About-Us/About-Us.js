9b97d9c08a7c85a7c72df7b6d5c30e07557d3c19
Namit Doshi <namitdoshi90@gmail.com>
2020-09-05 22:29:31 +0530

    about us component created

--- src/components/About-Us/About-Us.js
+++ src/components/About-Us/About-Us.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const AboutUs = () => {
+  var d = new Date();
+  var day = d.getDay();
+  const dayList = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
   return (
-    <div id='about-us'>
-      <p>About us component here</p>
+    <div id='about-us' className='container'>
+      <h2>Welcome to our site</h2>
+      <h3>We connect Job seekers to Recruiters and Recruiters to Job Seekers</h3>
+      <h4>Enjoy the rest of your {dayList[day]}</h4>
     </div>
   )
 }
