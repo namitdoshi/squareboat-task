e46fe4941269daba4a46029314df4aa76ced4524
Namit Doshi <namitdoshi90@gmail.com>
2020-09-05 22:31:24 +0530

    styling added to about-us component

--- src/components/About-Us/About-Us.js
+++ src/components/About-Us/About-Us.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const AboutUs = () => {
+
+  const styleSheet = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+
   var d = new Date();
   var day = d.getDay();
   const dayList = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
   return (
-    <div id='about-us' className='container'>
+    <div id='about-us' className='container' style={styleSheet}>
       <h2>Welcome to our site</h2>
       <h3>We connect Job seekers to Recruiters and Recruiters to Job Seekers</h3>
       <h4>Enjoy the rest of your {dayList[day]}</h4>
